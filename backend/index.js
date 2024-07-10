3e0a2c02d0421bf4202f63945c9fd3672b5a1d9a
Risha <rishamkeerthi@gmail.com>
2024-07-11 00:09:31 +0530

    set up mongodb connection and made the schemas

--- backend/index.js
+++ backend/index.js
@@ -1,72 +1,27 @@
-const cors = require('cors');
-const express = require('express');
-const mongoose = require('mongoose');
-const FormDataModel = require ('./models/FormData');
-const SerdataModel = require('./models/serdata');
+import express, { response } from "express";
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from "mongoose";
 
-
+// app is an instance of express application
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/bikeservice');
-
-app.post('/register', (req, res)=>{
-    // To post / insert data into database
-
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            res.json("Already registered")
-        }
-        else{
-            FormDataModel.create(req.body)
-            .then(log_reg_form => res.json(log_reg_form))
-            .catch(err => res.json(err))
-        }
-    })
-    
-})
-app.post('/firststep',(req, res)=>{
-    const {emailid, fullname} = req.body;
-    console.log("hello");
-    SerdataModel.findOne({emailid: emailid})
-    .then(user =>{
-        if(user){
-            console.log(user)
-            res.json("Already registered")
-        }
-        else{
-            SerdataModel.create(req.body)
-            .then(log_ser_form => res.json(log_ser_form))
-            .catch(err => res.json(err)) 
-        }
-    })
+//setting up the root request 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(201).send('Accepted');
 })
 
-app.post('/login', (req, res)=>{
-    // To find record from the database
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            // If user found then these 2 cases
-            if(user.password === password) {
-                res.json("Success");
-            }
-            else{
-                res.json("Wrong password");
-            }
-        }
-        // If user not found then 
-        else{
-            res.json("No records found! ");
-        }
+
+//Connect to database using Mongoose
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database successfully!');
+        // express application starts listening on PORT 5555 
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
     })
-})
-
-app.listen(3001, () => {
-    console.log("Server listining on http://127.0.0.1:3001");
-
-});
+    .catch((error) => {
+        console.log(error);
+    });
