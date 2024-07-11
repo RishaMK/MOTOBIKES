33c9fb942bff0fde96aa96d452d50fbd62baab2c
Risha <rishamkeerthi@gmail.com>
2024-07-11 19:33:35 +0530

    route to post

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,42 @@
 import express, { response } from "express";
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from "mongoose";
+import { Service } from "./models/ServiceData.js";
 
 // app is an instance of express application
 const app = express();
+
+//Middleware to parse request body
+app.use(express.json());
+
+//route to send data from service form to mongo
+app.post('/services', async (request, response) => {
+    try{
+
+        if(
+            !request.body.user_name ||
+            !request.body.user_email ||
+            !request.body.model ||
+            !request.body.service_type
+        ){
+            return response.status(400).send({message: "enter all required fields"});
+        }
+
+        const newService = {
+            user_name: request.body.user_name,
+            user_email: request.body.user_email,
+            model: request.body.model,
+            service_type: request.body.service_type,
+        };
+        const service = await Service.create(newService);
+        response.status(201).send(service);
+    }
+    catch(error){
+        console.log(error);
+        response.status(500).send({message:error.message});
+    }
+})
+
 
 //setting up the root request 
 app.get('/', (request, response) => {
