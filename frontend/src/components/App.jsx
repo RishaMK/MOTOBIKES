110252321608c04bfc97269066b38f32abc7dddb
Rajiv <rajiv.muttur@gmail.com>
2024-07-16 22:56:31 +0530

    Adding frontend changes. Created new pages for ContactUs, Services and created a card carousel

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -5,6 +5,11 @@ import Login from './Login';
 import Register from './Register';
 import FirstStep from './FirstStep';
 import Navbar from './Navbar';
+import {Routes, Route} from "react-router-dom";
+import History from './History';
+import Searchbar from './Searchbar';
+import ContactUs from './ContactUs';
+import Services from './Services';
 
 function App() {
   return (
@@ -12,11 +17,15 @@ function App() {
       <Navbar />
       <div style={{ marginTop: '3.5rem' }}>
         <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/firststep" element={<FirstStep />} />
+          <Route path="/" element ={<Register/>} />
+          <Route path="/register" element ={<Register/>} />
+          <Route path="/login" element ={<Login/>} />
+          <Route path="/home" element ={<Home/>} />
+          <Route path='/firststep' element={<FirstStep/>}/>
+          <Route path='/history' element={<History/>}/>
+          <Route path='/Searchbar' element={<Searchbar/>}/>
+          <Route path='/ContactUs' element={<ContactUs/>}/>
+          <Route path='/Services' element={<Services/>}/>
         </Routes>
       </div>
     </div>
