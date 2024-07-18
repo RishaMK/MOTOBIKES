54c67840fc7e03e4dc726304877ff856d1aa5052
Risha <rishamkeerthi@gmail.com>
2024-07-18 11:56:09 +0530

    small changes

--- frontend/src/components/HistoryCard.jsx
+++ frontend/src/components/HistoryCard.jsx
@@ -4,11 +4,6 @@ import './History.css'
 
 const HistoryCard = ({ user, email, model, service, requested, last_update, onDelete, id }) => {
   const navigate = useNavigate();
-
-  // const handleEdit = () => {
-  //   navigate(`/edit/${id}`);
-  // };
-
   return (
     <div className="record-content">
       <h4>User: {user}</h4>
@@ -17,7 +12,6 @@ const HistoryCard = ({ user, email, model, service, requested, last_update, onDe
       <h4>Service Type: {service}</h4>
       <h4>Requested: {requested}</h4>
       <h4>Last updated: {last_update}</h4>
-      {/* <button onClick={handleEdit}>Edit</button>  */}
       <button className='delete-card' onClick={() => onDelete(id)}>Delete</button>
     </div>
   );
